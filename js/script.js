0d2afe3989bb3782e6e4ba71b0c3607e4ffdf988
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-06-19 04:33:16 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     $(".menu_btn").click(function () {
+
         var element = document.getElementById("menu");
         element.style.display = (element.style.display == 'none') ? 'block' : 'none';
+
     });
+
 }); 
